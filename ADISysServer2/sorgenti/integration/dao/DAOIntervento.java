583b725a6a6bb8f1a4c07d0e3bef851cf057fd70
carlgauss <carlgauss@email.com>
2013-08-02 22:46:16 +0200

    Eliminati commenti "TODO" autogenerati da Eclipse.

--- ADISysServer2/sorgenti/integration/dao/DAOIntervento.java
+++ ADISysServer2/sorgenti/integration/dao/DAOIntervento.java
@@ -6,31 +6,31 @@ public class DAOIntervento implements DAO<Intervento> {
 
 	@Override
 	public Object create(Intervento entity) {
-		// TODO Auto-generated method stub
+
 		return null;
 	}
 
 	@Override
 	public Object update(Intervento entity) {
-		// TODO Auto-generated method stub
+
 		return null;
 	}
 
 	@Override
 	public Object delete(Intervento entity) {
-		// TODO Auto-generated method stub
+
 		return null;
 	}
 
 	@Override
 	public Object read(String ID) {
-		// TODO Auto-generated method stub
+
 		return null;
 	}
 
 	@Override
 	public Object getAll() {
-		// TODO Auto-generated method stub
+
 		return null;
 	}
 
--- ADISysServer2/sorgenti/integration/dao/DAOPaziente.java
+++ ADISysServer2/sorgenti/integration/dao/DAOPaziente.java
@@ -6,31 +6,31 @@ public class DAOPaziente implements DAO<Paziente> {
 
 	@Override
 	public Object create(Paziente entity) {
-		// TODO Auto-generated method stub
+
 		return null;
 	}
 
 	@Override
 	public Object update(Paziente entity) {
-		// TODO Auto-generated method stub
+
 		return null;
 	}
 
 	@Override
 	public Object delete(Paziente entity) {
-		// TODO Auto-generated method stub
+
 		return null;
 	}
 
 	@Override
 	public Object read(String ID) {
-		// TODO Auto-generated method stub
+
 		return null;
 	}
 
 	@Override
 	public Object getAll() {
-		// TODO Auto-generated method stub
+
 		return null;
 	}
 
