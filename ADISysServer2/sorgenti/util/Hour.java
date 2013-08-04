7202270ec10c0e4e2a83df31dafe68b0fceb9b60
Stafflute <Michaelius7@gmail.com>
2013-08-04 10:49:52 +0200

    Aggiunto Hour

---
+++ ADISysServer2/sorgenti/util/Hour.java
@@ -0,0 +1,10 @@
+package util;
+
+public class Hour {
+	/*
+	 * Qui il discorso relativo al numero dei parametri
+	 * si fa un pochino complicato perché Hour deve essere
+	 * immutabile, altrimenti ci possiamo incappare in
+	 * spiacevoli conseguenze.
+	 */
+}
