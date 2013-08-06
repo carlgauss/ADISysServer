2ca26fa05f253f45ffe14b6bc1709aef63fcb97b
Stafflute <Michaelius7@gmail.com>
2013-08-06 13:58:24 +0200

    implementato DAOInfermiere

--- ADISysServer2/sorgenti/integration/dao/DAOInfermiere.java
+++ ADISysServer2/sorgenti/integration/dao/DAOInfermiere.java
@@ -1,5 +1,8 @@
 package integration.dao;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.LinkedList;
 import java.util.List;
 
 import business.entity.Infermiere;
@@ -13,6 +16,12 @@ public class DAOInfermiere extends HQSQLDAO<Infermiere> {
 	//DELETE_QUERY not implemented
 	private static final String READ_QUERY = "SELECT ID, Nome, Cognome FROM Infermiere where ID = ?";
 	private static final String GET_ALL_QUERY = "SELECT ID, Nome, Cognome FROM Infermieri";
+	
+	private static final String ID_INFERMIERE_ATTRIBUTE_NAME = "ID";
+	private static final String NOME_INFERMIERE_ATTRIBUTE_NAME = "Nome";
+	private static final String COGNOME_INFERMIERE_ATTRIBUTE_NAME = "Cognome";
+	
+	private static final int FIRST = 0;
 
 	@Override
 	public void create(Infermiere entity) {
@@ -29,13 +38,16 @@ public class DAOInfermiere extends HQSQLDAO<Infermiere> {
 
 	@Override
 	public void update(Infermiere entity) {
-		String updateQuery = INSERT_QUERY;
+		String updateQuery = UPDATE_QUERY;
 		
 		String nomeInfermiere = entity.getNome();
 		updateQuery = queryReplaceFirst(updateQuery, nomeInfermiere);
 		
 		String cognomeInfermiere = entity.getNome();
 		updateQuery = queryReplaceFirst(updateQuery, cognomeInfermiere);
+		
+		String idInfermiere = entity.getId();
+		updateQuery = queryReplaceFirst(updateQuery, idInfermiere);
 		
 		connector.executeUpdateQuery(updateQuery);
 	}
@@ -47,14 +59,49 @@ public class DAOInfermiere extends HQSQLDAO<Infermiere> {
 
 	@Override
 	public Infermiere read(String ID) {
-
-		return null;
+		String readQuery = READ_QUERY;
+		
+		readQuery = queryReplaceFirst(readQuery, ID);
+		
+		ResultSet readQueryResultSet = connector.executeReadQuery(readQuery);
+		
+		List<Infermiere> elencoInfermieri = createElencoInfermieriBy(readQueryResultSet);
+		
+		return elencoInfermieri.get(FIRST);
 	}
 
 	@Override
 	public List<Infermiere> getAll() {
-
-		return null;
+		String getAllQuery = GET_ALL_QUERY;
+		
+		ResultSet getAllQueryResultSet = connector.executeReadQuery(getAllQuery);
+		
+		List<Infermiere> elencoInfermieri = createElencoInfermieriBy(getAllQueryResultSet);
+		
+		return elencoInfermieri;
 	}
-
+	private List<Infermiere> createElencoInfermieriBy(ResultSet resultSet) {
+		List<Infermiere> result = new LinkedList<Infermiere>();
+		
+		try {
+			while (resultSet.next()) {
+				Infermiere element = new Infermiere();
+				
+				String id = resultSet.getString(ID_INFERMIERE_ATTRIBUTE_NAME);
+				element.setId(id);
+				
+				String nome = resultSet.getString(NOME_INFERMIERE_ATTRIBUTE_NAME);
+				element.setNome(nome);
+				
+				String cognome = resultSet.getString(COGNOME_INFERMIERE_ATTRIBUTE_NAME);
+				element.setCognome(cognome);
+				
+				result.add(element);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return result;
+	}
 }
