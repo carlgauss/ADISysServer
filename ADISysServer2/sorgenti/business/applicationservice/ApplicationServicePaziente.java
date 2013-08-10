47881f388f4e7124a4ae5ec369568df57d31bbba
Stafflute <Michaelius7@gmail.com>
2013-08-10 23:54:23 +0200

    piccole modifiche

--- ADISysServer2/sorgenti/business/applicationservice/ApplicationServicePaziente.java
+++ ADISysServer2/sorgenti/business/applicationservice/ApplicationServicePaziente.java
@@ -1,12 +1,17 @@
 package business.applicationservice;
 
+import integration.dao.DAO;
+import integration.dao.DAOFactory;
+
 import java.util.List;
 
 import business.entity.Paziente;
 import presentation.controller.ApplicationService;
 import util.Parameter;
-//TODO tutti i metodi
+
 public class ApplicationServicePaziente implements ApplicationService, CRUG<Paziente> {
+	
+	private DAO<Paziente> daoInfermiere = DAOFactory.buildInstance("DAOPaziente");
 
     public void create(Parameter parameter) {
     	//return null;
