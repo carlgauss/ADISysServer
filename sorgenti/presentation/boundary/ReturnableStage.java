137f2d878375fe54d592f0207e54d9af36001688
stafflute <michaelius7@gmail.com>
2013-08-21 23:55:46 +0200

    pulito un po' di codice

--- sorgenti/presentation/boundary/ReturnableStage.java
+++ sorgenti/presentation/boundary/ReturnableStage.java
@@ -1,6 +1,7 @@
 package presentation.boundary;
 
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.layout.Region;
 import javafx.stage.Stage;
 import util.Parameter;
@@ -10,21 +11,27 @@ import java.io.IOException;
 import java.util.Properties;
 
 public abstract class ReturnableStage extends Stage implements Boundary {
+    private static final String MARKUP_FOLDER = "markup/";
+    private static final String FXML_EXTENSION = ".fxml";
+
     protected Parameter parameter;
     protected Object value;
     protected Region root;
     protected double width;
     protected double height;
+    protected Scene scene;
 
-    public ReturnableStage(Parameter parameter, String schemePath) {
+    public ReturnableStage(Parameter parameter, String schemeResource) {
         FXMLLoader fxmlLoader = new FXMLLoader();
 
         Properties properties = SimpleFormTranslator.getLanguage();
 
         Class<?> mainClass = getClass();
 
+        schemeResource = MARKUP_FOLDER + schemeResource + FXML_EXTENSION;
+
         try {
-            root = fxmlLoader.load(mainClass.getResource(schemePath));
+            root = fxmlLoader.load(mainClass.getResource(schemeResource));
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -33,6 +40,9 @@ public abstract class ReturnableStage extends Stage implements Boundary {
 
         width = root.getPrefWidth();
         height = root.getPrefHeight();
+
+        scene = new Scene(root, width, height);
+        setScene(scene);
 
         this.parameter = parameter;
     }
--- sorgenti/presentation/boundary/SchermataInserimentoInfermiere.java
+++ sorgenti/presentation/boundary/SchermataInserimentoInfermiere.java
@@ -1,6 +1,5 @@
 package presentation.boundary;
 
-import javafx.scene.Scene;
 import javafx.stage.Modality;
 import util.Parameter;
 import util.SimpleFormTranslator;
@@ -15,21 +14,14 @@ import util.SimpleFormTranslator;
 
 //TODO
 public class SchermataInserimentoInfermiere extends ReturnableStage {
-    private static final String MARKUP_FOLDER = "markup/";
-    private static final String FXML_EXTENSION = ".fxml";
     private static final String SCHEME_RESOURCE = "SchermataImmissioneInfermiere";
 
-    private static final String SCHEME_PATH = MARKUP_FOLDER + SCHEME_RESOURCE + FXML_EXTENSION;
-
     public SchermataInserimentoInfermiere(Parameter parameter) {
-        super(parameter, SCHEME_PATH);
+        super(parameter, SCHEME_RESOURCE);
 
         setTitle(SimpleFormTranslator.translate("addNurseScreen"));
         setResizable(false);
 
         initModality(Modality.APPLICATION_MODAL);
-        Scene scene = new Scene(root, width, height);
-
-        setScene(scene);
     }
 }
