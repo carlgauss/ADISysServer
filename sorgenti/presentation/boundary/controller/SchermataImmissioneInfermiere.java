df81512171d4207ad7eb0728fee3467f2762c032
stafflute <michaelius7@gmail.com>
2013-08-22 12:46:01 +0200

    altre modifiche

--- sorgenti/presentation/boundary/controller/SchermataImmissioneInfermiere.java
+++ sorgenti/presentation/boundary/controller/SchermataImmissioneInfermiere.java
@@ -1,11 +1,15 @@
 package presentation.boundary.controller;
 
 import business.entity.Infermiere;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.control.Labeled;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 import presentation.boundary.ReturnableStage;
+import presentation.controller.FrontController;
+import presentation.controller.FrontControllerFactory;
 import util.SimpleLabelTranslator;
 
 import java.net.URL;
@@ -15,12 +19,23 @@ public class SchermataImmissioneInfermiere extends SchermataImmissione {
     private ReturnableStage stage;
     private boolean isEdit;
 
+    private FrontController fc = FrontControllerFactory.buildInstance();
+
     @FXML private Node root;
     @FXML private Labeled titolo;
 
     @FXML private Labeled id;
     @FXML private TextField nome;
     @FXML private TextField cognome;
+
+    @FXML private void onOk(ActionEvent event) {
+        System.out.println("aaaaa");
+    }
+
+    @FXML private void onCancel(ActionEvent event) {
+        System.out.println("bbbbb");
+        getStage().close();
+    }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
@@ -41,5 +56,9 @@ public class SchermataImmissioneInfermiere extends SchermataImmissione {
         id.setText(infermiere.getId());
         nome.setText(infermiere.getNome());
         cognome.setText(infermiere.getCognome());
+    }
+
+    private Stage getStage() {
+        return (Stage) root.getScene().getWindow();
     }
 }
